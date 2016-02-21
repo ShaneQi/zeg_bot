4526ee820d94624f4cc6b11323da3b388c2bfb9f
qizt <qiztzone@126.com>
2016-02-21 21:54:49 +0000

    V1.1 1. Add /kr. 2. Now zeg_bot only reply to the message that are replied by summons.    (If summons dont't reply to any messages, zeg_bot will send the respond replying to nothing)

--- index.php
+++ index.php
@@ -8,14 +8,31 @@
   $text = $updateArray["message"]["text"];
   $chatId = $updateArray["message"]["chat"]["id"];
   $messageId = $updateArray["message"]["message_id"];
+  $replyToMessageID = $updateArray["message"]["reply_to_message"]["message_id"];
 
   //  Jake.
   if (stripos($text, '/jake') !== false) {
-    $jakeReply = $website."/sendMessage?chat_id=".$chatId."&reply_to_message_id=".$messageId."&text=🙄️";
+    if ($replyToMessageID) {
+      $jakeReply = $website."/sendMessage?chat_id=".$chatId."&reply_to_message_id=".$replyToMessageID."&text=🙄️";
+    } else {
+      $jakeReply = $website."/sendMessage?chat_id=".$chatId."&text=🙄️";
+    }
     $sentMessage = file_get_contents($jakeReply);
   }
 
+  //  Kr.
+  if (stripos($text, '/kr') !== false) {
+    if ($replyToMessageID) {
+      $krReply = $website."/sendSticker?chat_id=".$chatId."&reply_to_message_id=".$replyToMessageID."&sticker=BQADBQADogIAAmdqYwStx5TlmCMy0gI";
+    } else {
+      $krReply = $website."/sendSticker?chat_id=".$chatId."&sticker=BQADBQADogIAAmdqYwStx5TlmCMy0gI";
+    }
+    $sentMessage = file_get_contents($krReply);
+  }
+
+//  if ($replyToMessageID) { $tof = 1; }
+//  else { $tof = 2; }
 //  $myfile = fopen("debug", "w") or die("Unable to open file!");
-//  fwrite($myfile, $jakeReply);
+//  fwrite($myfile, $tof);
 //  fclose($myfile);
 ?>
