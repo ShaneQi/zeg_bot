e3cbe3f6621852c18374fdd489df27e4f22a07cf
qizt <qiztzone@126.com>
2016-02-22 02:37:43 +0000

    Add photo archive feature.

--- index.php
+++ index.php
@@ -8,24 +8,38 @@
   $text = $updateArray["message"]["text"];
   $chatId = $updateArray["message"]["chat"]["id"];
   $messageId = $updateArray["message"]["message_id"];
+  $messageDateId = $updateArray["message"]["date"];
   $replyToMessageID = $updateArray["message"]["reply_to_message"]["message_id"];
+  $photoFileId = $updateArray["message"]["photo"][1]["file_id"]; 
+
+  //  Photo.
+  if ($photoFileId) {
+    $getPhoto = $website."/getFile?file_id=".$photoFileId;
+    $photo = file_get_contents($getPhoto);
+    $photoArray = json_decode($photo, TRUE);
+    $photoFilePath = $photoArray["result"]["file_path"];
+    $photoUrl = "https://api.telegram.org/file/bot".$botToken."/".$photoFilePath;
+
+    file_put_contents(realpath("./photo").'/'.$messageDateId.'.jpg', file_get_contents($photoUrl));
+
+    $savePhotoReply = $website."/sendMessage?chat_id=".$chatId."&text=You da driver!"."&reply_to_message_id=".$messageId;
+    $sentMessage = file_get_contents($savePhotoReply);
+  }
 
   //  Jake.
   if (stripos($text, '/jake') !== false) {
+    $jakeReply = $website."/sendMessage?chat_id=".$chatId."&text=🙄️";
     if ($replyToMessageID) {
-      $jakeReply = $website."/sendMessage?chat_id=".$chatId."&reply_to_message_id=".$replyToMessageID."&text=🙄️";
-    } else {
-      $jakeReply = $website."/sendMessage?chat_id=".$chatId."&text=🙄️";
+      $jakeReply = $jakeReply."&reply_to_message_id=".$replyToMessageID;
     }
     $sentMessage = file_get_contents($jakeReply);
   }
 
   //  Kr.
   if (stripos($text, '/kr') !== false) {
+    $krReply = $website."/sendSticker?chat_id=".$chatId."&sticker=BQADBQADogIAAmdqYwStx5TlmCMy0gI";
     if ($replyToMessageID) {
-      $krReply = $website."/sendSticker?chat_id=".$chatId."&reply_to_message_id=".$replyToMessageID."&sticker=BQADBQADogIAAmdqYwStx5TlmCMy0gI";
-    } else {
-      $krReply = $website."/sendSticker?chat_id=".$chatId."&sticker=BQADBQADogIAAmdqYwStx5TlmCMy0gI";
+      $krReply = $krReply."&reply_to_message_id=".$replyToMessageID;
     }
     $sentMessage = file_get_contents($krReply);
   }
@@ -33,6 +47,6 @@
 //  if ($replyToMessageID) { $tof = 1; }
 //  else { $tof = 2; }
 //  $myfile = fopen("debug", "w") or die("Unable to open file!");
-//  fwrite($myfile, $tof);
+//  fwrite($myfile, $photoArray);
 //  fclose($myfile);
 ?>
